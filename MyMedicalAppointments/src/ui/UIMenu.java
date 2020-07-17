dc4435a8c2455e41adcc59026e792171f447083d
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-07-17 18:59:32 +0200

    Variables estaticas constantes

--- MyMedicalAppointments/src/ui/UIMenu.java
+++ MyMedicalAppointments/src/ui/UIMenu.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class UIMenu {
 
+    public static final  String[] MONTHS = {"Enero", "Febrero", "Marzo",
+            "Abril", "Mayo", "Junio", "Julio",
+            "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opcion deseada");
@@ -44,6 +48,9 @@ public class UIMenu {
             switch (response){
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 1; i < 4; i++) {
+                        System.out.println(i+". "+MONTHS[i-1]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
